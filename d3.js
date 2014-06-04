6ac59855907cc46695a12ca83e9c3515244ab6af
Toshihiko Heike <heike@akademia.co.jp>
2014-06-04 18:48:04 +0900

    Add star shape symbol to d3.svg.symbol

--- d3.js
+++ d3.js
@@ -8349,6 +8349,21 @@
     "triangle-up": function(size) {
       var rx = Math.sqrt(size / d3_svg_symbolSqrt3), ry = rx * d3_svg_symbolSqrt3 / 2;
       return "M0," + -ry + "L" + rx + "," + ry + " " + -rx + "," + ry + "Z";
+    },
+    star: function(size) {
+      var r = Math.sqrt(size / d3_svg_symbolSqrt3), inr = r * Math.sin(π / 10) / Math.sin(7 * π / 10), output = "M";
+      function x(val) {
+        return (val % 2 == 0 ? r : inr) * Math.cos(val * π / 5 - π / 2);
+      }
+      function y(val) {
+        return (val % 2 == 0 ? r : inr) * Math.sin(val * π / 5 - π / 2);
+      }
+      for (var i = 0; i < 10; i++) {
+        if (i != 0) output += "L";
+        output += x(i) + "," + y(i);
+      }
+      output += "Z";
+      return output;
     }
   });
   d3.svg.symbolTypes = d3_svg_symbols.keys();
--- src/svg/symbol.js
+++ src/svg/symbol.js
@@ -96,6 +96,24 @@ var d3_svg_symbols = d3.map({
         + "L" + rx +"," + ry
         + " " + -rx + "," + ry
         + "Z";
+  }, 
+  "star": function(size) { 
+    //var r = Math.sqrt(size / π); 
+    var r = Math.sqrt(size / d3_svg_symbolSqrt3), 
+        inr = r * Math.sin(π / 10) / Math.sin(7 * π / 10), 
+        output = "M"; 
+    function x(val) { 
+      return (val % 2 == 0 ? r : inr) * Math.cos(val * π / 5 - π / 2); 
+    } 
+    function y(val) { 
+      return (val % 2 == 0 ? r : inr) * Math.sin(val * π / 5 - π / 2); 
+    } 
+    for (var i = 0; i < 10; i++) { 
+      if (i!=0) output += "L"; 
+      output += (x(i)+","+y(i)); 
+    } 
+    output += "Z"; 
+    return output;
   }
 });
 
